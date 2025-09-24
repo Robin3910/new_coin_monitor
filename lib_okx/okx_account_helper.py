cdf7ab7e6e90c94eb53ac48d7ee33efb5912e50b
robin3910 <robin3910@163.com>
2025-09-24 17:19:11 +0800

    update 关闭无用的微信通知

--- lib_okx/okx_account_helper.py
+++ lib_okx/okx_account_helper.py
@@ -583,7 +583,7 @@ class OkxAccountHelper:
                                           f"倒数第三根K线跌幅: {((prev_prev_close-prev_prev_open)/prev_prev_open*100):.2f}%\n" \
                                           f"当前资金费率: {funding_rate*100:.2f}%\n" \
                                           f"当前标记价格: {mark_price}"
-                                self.send_wx_notification(f"okx-{symbol}空", alert_msg)
+                                # self.send_wx_notification(f"okx-{symbol}空", alert_msg)
                                 # 获取账户余额
                                 balance_info = accountAPI.get_account_balance()
                                 available_balance = 0
